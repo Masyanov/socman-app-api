91f051b9463b3fe2a399ac9198751bf526ac181e
Masyanov <masyanov.aleksei@gmail.com>
2024-03-25 00:57:03 +0300

    update field for registrations

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,27 +4,27 @@
 
         <!-- Name -->
         <div>
-            <x-input-label for="name" :value="__('Name')" />
+            <x-input-label for="name" :value="__('Имя')" />
             <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
             <x-input-error :messages="$errors->get('name')" class="mt-2" />
         </div>
         <!-- Last Name -->
         <div class="mt-4">
-            <x-input-label for="last_name" :value="__('Last Name')" />
+            <x-input-label for="last_name" :value="__('Фамилия')" />
             <x-text-input id="last_name" class="block mt-1 w-full" type="text" name="last_name" :value="old('last_name')" required autocomplete="last_name" />
             <x-input-error :messages="$errors->get('last_name')" class="mt-2" />
         </div>
         <!-- Role -->
         <div class="mt-4">
-            <x-input-label for="role" :value="__('Role')" />
+            <x-input-label for="role" :value="__('Роль')" />
 
             <ul class="grid w-full mt-1 gap-6 md:grid-cols-2">
                 <li>
                     <input type="radio" id="player" name="role" value="player" class="hidden peer role" required checked/>
                     <label for="player" class="inline-flex items-center justify-between w-full p-5 text-gray-500 bg-white border border-gray-200 rounded-lg cursor-pointer dark:hover:text-gray-300 dark:border-gray-700 dark:peer-checked:text-blue-500 peer-checked:border-blue-600 peer-checked:text-blue-600 hover:text-gray-600 hover:bg-gray-100 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700">
                         <div class="block">
-                            <div class="w-full text-lg font-semibold">{{ __('Player') }}</div>
-                            <div class="w-full">{{ __('Registration as player') }}</div>
+                            <div class="w-full text-lg font-semibold">{{ __('Игрок') }}</div>
+                            <div class="w-full" style="font-size: 12px;">{{ __('Регистрация как игрок') }}</div>
                         </div>
                         <svg class="w-5 h-5 ms-3 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
@@ -35,8 +35,8 @@
                     <input type="radio" id="coach" name="role" value="coach" class="hidden peer role">
                     <label for="coach" class="inline-flex items-center justify-between w-full p-5 text-gray-500 bg-white border border-gray-200 rounded-lg cursor-pointer dark:hover:text-gray-300 dark:border-gray-700 dark:peer-checked:text-blue-500 peer-checked:border-blue-600 peer-checked:text-blue-600 hover:text-gray-600 hover:bg-gray-100 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700">
                         <div class="block">
-                            <div class="w-full text-lg font-semibold">{{ __('Coach') }}</div>
-                            <div class="w-full">{{ __('Registration as coach') }}</div>
+                            <div class="w-full text-lg font-semibold">{{ __('Тренер') }}</div>
+                            <div class="w-full" style="font-size: 12px;">{{ __('Регистрация как тренер') }}</div>
                         </div>
                         <svg class="w-5 h-5 ms-3 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
@@ -50,7 +50,7 @@
 
         <!-- Team Code -->
         <div class="mt-4" id="code_field">
-            <x-input-label for="team_code" :value="__('Team code')" />
+            <x-input-label for="team_code" :value="__('Код команды')" />
             <x-text-input id="team_code" class="block mt-1 w-full team_code" type="text" name="team_code" :value="old('team_code')" autocomplete="team_code" placeholder="AAA-999" />
             <x-input-error :messages="$errors->get('team_code')" class="mt-2" />
         </div>
@@ -64,7 +64,7 @@
 
         <!-- Password -->
         <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+            <x-input-label for="password" :value="__('Пароль')" />
 
             <x-text-input id="password" class="block mt-1 w-full"
                             type="password"
@@ -76,7 +76,7 @@
 
         <!-- Confirm Password -->
         <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
+            <x-input-label for="password_confirmation" :value="__('Подтверждения пароля')" />
 
             <x-text-input id="password_confirmation" class="block mt-1 w-full"
                             type="password"
@@ -87,11 +87,11 @@
 
         <div class="flex items-center justify-end mt-4">
             <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('login') }}">
-                {{ __('Already registered?') }}
+                {{ __('Еще на зарегистрированы?') }}
             </a>
 
             <x-primary-button class="ms-4">
-                {{ __('Register') }}
+                {{ __('Регистрация') }}
             </x-primary-button>
         </div>
     </form>
